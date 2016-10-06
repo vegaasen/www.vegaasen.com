3e2f8104d217eeca516132288a50d1adc651c640
vegaasen <vegaasen@gmail.com>
2016-10-06 11:44:34 +0200

    Refactored all references to javascripts and other sources. Streamlines

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,7 @@
-;(function(){
-
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
-
-
-})(jQuery)
+;(function () {
+    $('#menuToggle, .menu-close').on('click', function () {
+        $('#menuToggle').toggleClass('active');
+        $('body').toggleClass('body-push-toleft');
+        $('#theMenu').toggleClass('menu-open');
+    });
+})(jQuery);
