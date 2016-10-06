c5227cedab31dd3d8aee9a2baac2bdb043738f62
vegaasen <vegaasen@gmail.com>
2016-10-06 08:52:02 +0200

    Converted php -> html

--- src/static/Config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$config['database']['type'] = 'mysql';
-$config['database']['host'] = '-';
-$config['database']['username'] = '-';
-$config['database']['password'] = '-';
-$config['database']['database'] = '-';
-$config['application']['title'] = "vegaasen.com";
-$config['application']['version'] = "6.0.1-SNAPSHOT";
-?>
