51a665a931ae09fb9eb7d4d7fa78f26e1541e500
vegaasen <vegaasen@gmail.com>
2017-03-21 19:26:52 +0100

    VEG-000:Misc.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,9 @@ eval "$(ssh-agent -s)";
 # creates
 echo -e "$SSH_DEPLOYMENT_KEY" > ${SSH_KEY_NAME};
 
-echo "Key-file created. Changing access to the key";
+echo "Key-file created ---v. Changing access to the key";
+
+cat ${SSH_KEY_NAME};
 
 chmod 600 ${SSH_KEY_NAME};
 
@@ -21,6 +23,6 @@ ssh-add ${SSH_KEY_NAME};
 
 echo "Transferring files using rsync";
 
-rsync -r --delete-after --quiet -avz -e "ssh -i ${SSH_KEY_NAME}" $TRAVIS_BUILD_DIR/dist $SSH_HOST_USER_USERNAME@$SSH_HOST_IP:$deploy-location;
+rsync -r --delete-after --quiet -avz -e "ssh -i ${SSH_KEY_NAME}" ${TRAVIS_BUILD_DIR}/dist ${SSH_HOST_USER_USERNAME}@${SSH_HOST_IP}:${SSH_DEPLPOYMENT_LOCATION};
 
 echo "Transfer completed to {$deploy-location} :-)";
