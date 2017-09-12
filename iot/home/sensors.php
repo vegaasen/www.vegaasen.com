53dfd6f383665b01cce218802aa88eb8c71145ee
vegaasen <vegaasen@gmail.com>
2017-09-12 23:00:06 +0200

    VEG-000: Minor details regarding the sensors thing

--- iot/home/sensors.php
+++ iot/home/sensors.php
@@ -64,7 +64,7 @@
 <div class="footer-helper"></div>
 <footer class="footer">
     <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+        <span class="text-muted">vegaasen pointless IOT playground 2o17</span>
     </div>
 </footer>
 <script src="//code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
