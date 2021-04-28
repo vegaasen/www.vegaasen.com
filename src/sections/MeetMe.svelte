665c48bacf990c01b98dc22cd3d7a1c13c931d30
Vegard Aasen <vegard.aasen@webstep.no>
2021-04-28 17:44:07 +0200

    Update dependencies to latest and greatest

--- src/sections/MeetMe.svelte
+++ src/sections/MeetMe.svelte
@@ -31,14 +31,14 @@
   <section class="developer">
     <SectionHeaded title="meet me" titleAsLeft="{false}">
       <div class="grid-box-double">
-        <div class="grid-element"><Card title="Road cycling" backgroundUrl="/images/IMG_5010.JPG" /></div>
-        <div class="grid-element"><Card title="Mountain biking" backgroundUrl="/images/IMG_5010.JPG" /></div>
-        <div class="grid-element"><Card title="Coding" backgroundUrl="/images/IMG_5010.JPG" /></div>
-        <div class="grid-element"><Card title="Running" size="large" backgroundUrl="/images/IMG_5010.JPG" /></div>
-        <div class="grid-element"><Card title="Cars &amp; coffee" backgroundUrl="/images/IMG_5010.JPG" /></div>
-        <div class="grid-element"><Card title="Baking" backgroundUrl="/images/IMG_5010.JPG" /></div>
-        <div class="grid-element"><Card title="Alpine &amp; snow" size="large" backgroundUrl="/images/IMG_5010.JPG" /></div>
-        <div class="grid-element"><Card title="Gaming" backgroundUrl="/images/IMG_5010.JPG" /></div>
+        <div class="grid-element"><Card title="Road cycling" backgroundUrl="/images/IMG_5010.jpg" /></div>
+        <div class="grid-element"><Card title="Mountain biking" backgroundUrl="/images/IMG_5010.jpg" /></div>
+        <div class="grid-element"><Card title="Coding" backgroundUrl="/images/IMG_5010.jpg" /></div>
+        <div class="grid-element"><Card title="Running" size="large" backgroundUrl="/images/IMG_5010.jpg" /></div>
+        <div class="grid-element"><Card title="Cars &amp; coffee" backgroundUrl="/images/IMG_5010.jpg" /></div>
+        <div class="grid-element"><Card title="Baking" backgroundUrl="/images/IMG_5010.jpg" /></div>
+        <div class="grid-element"><Card title="Alpine &amp; snow" size="large" backgroundUrl="/images/IMG_5010.jpg" /></div>
+        <div class="grid-element"><Card title="Gaming" backgroundUrl="/images/IMG_5010.jpg" /></div>
       </div>
     </SectionHeaded>
   </section>
