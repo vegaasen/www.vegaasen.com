70d43b6705e3fb2f7d75dd74d7fdf26c7e84c51a
Vegard Aasen <vegaasen@gmail.com>
2024-03-21 20:55:58 +0100

    ✨ Add responsive cues and add a small section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-    },
+    screens: {
+      's': '350px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   plugins: [],
 }
