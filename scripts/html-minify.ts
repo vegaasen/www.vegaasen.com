0d27fa43d1bb5677ae51809cd92c4da994f15d61
Vegard Aasen <vegaasen@gmail.com>
2026-03-20 10:16:27 +0100

    🛠️ Inline all CSS to reduce payload size

--- scripts/html-minify.ts
+++ scripts/html-minify.ts
@@ -4,10 +4,15 @@ import { readFile, writeFile } from 'node:fs/promises'
 const { minify } = minifyHtml
 
 const DEFAULT_BASE_FOLDER = 'src'
+const CSS_LINK_TAG = '<link href="./v.min.css" rel="stylesheet">'
 
 try {
-  const indexHtml = await readFile(`${DEFAULT_BASE_FOLDER}/index.html`)
-  const minified: Uint8Array = minify(indexHtml, {
+  const [indexHtml, css] = await Promise.all([
+    readFile(`${DEFAULT_BASE_FOLDER}/index.html`, 'utf-8'),
+    readFile(`${DEFAULT_BASE_FOLDER}/v.min.css`, 'utf-8'),
+  ])
+  const htmlWithInlinedCss = indexHtml.replace(CSS_LINK_TAG, `<style>${css}</style>`)
+  const minified: Uint8Array = minify(Buffer.from(htmlWithInlinedCss), {
     keep_closing_tags: true,
     minify_css: true,
     minify_js: true,
